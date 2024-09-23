3696e458a8c06b3b3041db2e665a96eddea0a658
Onika Chorba <110539259+onikaChorba@users.noreply.github.com>
2024-09-23 14:53:18 +0300

    style: variables, text

--- craco.config.js
+++ craco.config.js
@@ -7,4 +7,28 @@ module.exports = {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "postcss-loader",
+          {
+            loader: "resolve-url-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
