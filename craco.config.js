e0b90cd87ba855c02b8f12c1637c16f7ecff64cf
Onika Chorba <110539259+onikaChorba@users.noreply.github.com>
2024-09-24 10:41:53 +0300

    style: button

--- craco.config.js
+++ craco.config.js
@@ -2,34 +2,14 @@
 const path = require(`path`);
 
 module.exports = {
-  webpack: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          "style-loader",
-          "css-loader",
-          "postcss-loader",
-          {
-            loader: "resolve-url-loader",
-            options: {
-              sourceMap: true,
-            },
-          },
-          {
-            loader: "sass-loader",
-            options: {
-              sourceMap: true,
-              fiber: false,
-            },
-          },
-        ],
+  style: {
+    sass: {
+      loaderOptions: {
+        implementation: require("sass"),
+        sassOptions: {
+          api: "modern-compiler",
+        },
       },
-    ],
+    },
   },
 };
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -10,20 +10,20 @@ export const Header = () => {
         <img src={logo} alt="logo" /><p className='poppins-bold'>Chef in you</p>
       </div>
       <nav className='header__nav poppins-semibold'>
-        <a href="#">Home</a>
-        <a href="#">Recipes</a>
-        <a href="#">Add Recipe</a>
-        <a href="#">Blog</a>
+        <a href="/">Home</a>
+        <a href="/recipes">Recipes</a>
+        <a href="/add-recipe">Add Recipe</a>
+        <a href="/blog">Blog</a>
       </nav>
       <div className='header__buttons'>
         <Button
           text="Login in"
           onClick={() => console.log(1)}
-          className="button-secondary" />
+          className="button-secondary poppins-bold" />
         <Button
           text="Sing up"
           onClick={() => console.log(2)}
-          className="button-primary" />
+          className="button-primary poppins-bold" />
       </div>
     </header>
   )
