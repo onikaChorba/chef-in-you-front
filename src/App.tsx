c1695752ac246a6e7d2cd97646de127232ad8da4
Onika Chorba <110539259+onikaChorba@users.noreply.github.com>
2024-10-02 11:53:13 +0300

    feat: add filling in add recipe form

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,17 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Route, Routes } from "react-router-dom";
-import { useDispatch, useSelector } from 'react-redux';
 import { Header } from './components/header/header.tsx';
 import { Home } from './components/pages/home/home.tsx';
 import { Registration } from './components/form/registration/registration.tsx';
 import { Login } from './components/form/login/login.tsx';
 import { RecipeDetails } from './components/recipe/recipe.tsx';
-import { fetchAuthMe, selectIsAuth } from './redux/slices/auth.ts';
-import { AppDispatch } from './redux/store.ts';
 import { AddRecipe } from './components/pages/recipes/addRecipe.tsx';
 
 function App() {
-  const dispatch: AppDispatch = useDispatch();
-  const isAuth = useSelector(selectIsAuth);
-
-  useEffect(() => {
-    dispatch(fetchAuthMe());
-  });
 
   return (
-    <>
+    <div className='container'>
       <Header />
-      {isAuth}
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/recipes' element={<>Recipes</>} />
@@ -33,7 +23,7 @@ function App() {
         <Route path='/me' element={<>me</>} />
         <Route path='/blog' element={<>blog</>} />
       </Routes>
-    </>
+    </div>
   );
 }
 
--- src/components/pages/recipes/addRecipe.tsx
+++ src/components/pages/recipes/addRecipe.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectIsAuth } from "../../../redux/slices/auth.ts";
@@ -8,6 +8,46 @@ import { Input } from "../../input/input.tsx";
 export const AddRecipe = () => {
   const isAuth = useSelector(selectIsAuth);
 
+  const [ingredients, setIngredients] = useState([{ value: "" }]);
+  const [instructions, setInstructions] = useState([{ value: "" }]);
+  const addNewIngridientInput = () => {
+    setIngredients([...ingredients, { value: "" }]);
+  };
+
+  const removeIngredient = (index) => {
+    const newIngredients = ingredients.filter((_, i) => i !== index);
+    setIngredients(newIngredients);
+  };
+
+  const handleChangeIngredient = (e, index) => {
+    const newIngredients = [...ingredients];
+    newIngredients[index].value = e.target.value;
+
+    if (e.target.value && index === ingredients.length - 1) {
+      addNewIngridientInput();
+    }
+
+    setIngredients(newIngredients);
+  };
+  const addNewInstructionInput = () => {
+    setInstructions([...instructions, { value: "" }]);
+  };
+
+  const removeInstruction = (index) => {
+    const newIngredients = instructions.filter((_, i) => i !== index);
+    setInstructions(newIngredients);
+  };
+
+  const handleChangeInstruction = (e, index) => {
+    const newIngredients = [...instructions];
+    newIngredients[index].value = e.target.value;
+
+    if (e.target.value && index === instructions.length - 1) {
+      addNewInstructionInput();
+    }
+
+    setInstructions(newIngredients);
+  };
   if (!isAuth) {
     return <Navigate to="/" />
   };
@@ -21,15 +61,80 @@ export const AddRecipe = () => {
           <p className="poppins-regular">
             Create and share your favorite recipes with others! Add your unique recipe to inspire cooks from all over the world.
           </p>
-          <Button text="Create New Recipe" className="button-primary poppins-bold" onClick={() => console.log('create')} />
         </div>
       </div>
 
       <div>
         <Input name="recipe-title" label="Recipe title:" placeholder="Enter your recipe title" type="text" />
         <Input name="recipe-description" label="Description:" placeholder="Introduce your recipe" type="text" />
-        <Input name="recipe-ingredients" label="Ingredients:" placeholder="Add ingredients" type="text" />
-        <button> + ingredient </button>
+        <div>
+          <p style={{
+            fontSize: "16px",
+            fontWeight: "bold",
+            color: "#b55d51",
+            marginBottom: "8px"
+          }}> Ingredients:
+          </p>
+
+          {ingredients.map((ingredient, index) => (
+            <div key={index} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              <div style={{ width: '100%' }} className="input-container">
+                <input
+                  type="text"
+                  id={`ingredient-${index}`}
+                  name={`ingredient-${index}`}
+                  placeholder="Add ingredient"
+                  value={ingredient.value}
+                  onChange={(e) => handleChangeIngredient(e, index)}
+                />
+              </div>
+              <div>
+                <button type="button" onClick={() => removeIngredient(index)}>
+                  x
+                </button>
+              </div>
+            </div>
+          ))}
+          <button type="button" onClick={addNewIngridientInput}>
+            Add Another Ingredient
+          </button>
+
+          <p style={{
+            fontSize: "16px",
+            fontWeight: "bold",
+            color: "#b55d51",
+            marginBottom: "8px"
+          }}> Instructions:
+          </p>
+          {instructions.map((ingredient, index) => (
+            <div key={index} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              <div style={{ width: '100%' }} className="input-container">
+                <label htmlFor={`ingredient-${index}`}>Step {index + 1}:</label>
+                <input
+                  type="text"
+                  id={`ingredient-${index}`}
+                  name={`ingredient-${index}`}
+                  placeholder="Write instruction"
+                  value={ingredient.value}
+                  onChange={(e) => handleChangeInstruction(e, index)}
+                />
+              </div>
+              <div>
+                <button type="button" onClick={() => removeInstruction(index)}>
+                  x
+                </button>
+              </div>
+            </div>
+          ))}
+          <button type="button" onClick={addNewInstructionInput}>
+            Add Another Step
+          </button>
+        </div>
+        <Input name="recipe-servings" label="Servings:" placeholder="How many portions does this recipe make?" type="number" />
+        <Input name="recipe-time" label="Cooking time:" placeholder="How long does it take to prepate this recipe?" type="number" />
+        <Input name="recipe-tag" label="Tags" placeholder="Tags" type="text" />
+
+        <Button text="Create New Recipe" className="button-primary poppins-bold" onClick={() => console.log('create')} />
       </div>
     </>
   )
