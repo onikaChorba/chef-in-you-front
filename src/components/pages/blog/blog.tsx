4175ce193bc2e405d25649a7b1e18d130ea2bbac
Onika Chorba <110539259+onikaChorba@users.noreply.github.com>
2024-12-26 21:26:52 +0200

    style: article-block

--- src/components/pages/blog/blog.tsx
+++ src/components/pages/blog/blog.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from './blog.module.scss';
 import articleTop from '../../../assets/imgs/article-top.png'
+import cheesecake from '../../../assets/imgs/cheesecake.jpeg'
+import impact from '../../../assets/imgs/impact.jpg'
 import { Button } from "../../button/button";
 export const Blog = () => {
   return (
@@ -18,6 +20,30 @@ export const Blog = () => {
         </div>
         <div className={styles["article-top__img"]}>
           <img src={articleTop} alt="food" />
+        </div>
+      </div>
+
+      <div className={styles.articles}>
+        <div className={styles['article-block']}>
+          <div className={styles["article-block__img"]}>
+            <img src={cheesecake} alt="food" />
+          </div>
+          <div className={`${styles["article-block__data"]} poppins-light`} >on September 19, 2032</div>
+          <h3 className={`${styles["article-block__title"]} poppins-semibold`}>Classic New York_style Cheesecake Recipe</h3>
+          <p className={`${styles["article-block__text"]} poppins-regular`}>A Classic New York-style Cheesecake is the ultimate dessert for special occasions or any time you’re craving something indulgent. Known for its rich, creamy texture and slightly tangy flavor, this cheesecake is baked to perfection with a buttery graham cracker crust. The key to mastering this recipe lies in balancing the sweetness of cream cheese, sugar, and vanilla with the smoothness of sour cream and eggs.
+          </p>
+          <Button className="button-link" text="Read more" textStyle="" />
+        </div>
+        <div className={styles['article-block']}>
+          <div className={styles["article-block__img"]}>
+            <img src={impact} alt="food" />
+          </div>
+          <div className={`${styles["article-block__data"]} poppins-light`} >on December 25, 2024</div>
+          <h3 className={`${styles["article-block__title"]} poppins-semibold`}>The Surprising Effects of Sugar on Mental Health</h3>
+          <p className={`${styles["article-block__text"]} poppins-regular`}>
+            Sugar, often a staple in our daily diets, can have profound effects on mental health. Studies show that high sugar intake may lead to mood swings, anxiety, and even depression by affecting the body’s insulin levels and brain chemistry. While sweet treats can provide a temporary boost of energy, their long-term impact on mental well-being is less sweet.
+          </p>
+          <Button className="button-link" text="Read more" textStyle="" />
         </div>
       </div>
     </div>
